b3f3feebf92bcd25daf7264a57a72c0b5a59d8ff
Jason Gutierrez <jasong84@gmail.com>
2020-12-21 10:31:41 -0600

    Add two more helper functions

--- src/util/lib.js
+++ src/util/lib.js
@@ -1,8 +1,9 @@
 const sumArray = arr => arr.reduce( (a,b) => a + b, 0)
 const combineChars = arr => arr.reduce( (a,b) => a.concat(b), "")
 const parseStrNums = arr => arr.map(Number);
-
+const sumOfValues = (a,b) => a + b
+const productOfValues = (a,b) => a * b
 
 module.exports = {
-    sumArray, combineChars, parseStrNums
+    sumArray, combineChars, parseStrNums, sumOfValues, productOfValues
 }
